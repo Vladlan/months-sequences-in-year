dd6ebaa9e9b964a6c8b2075b0c7917b25b33d920
Vladlan <vladislav_lanskikh@epam.com>
2022-01-04 13:55:24 +0300

    moved code to a function

--- months.py
+++ months.py
@@ -1,14 +1,20 @@
-months = [1,2,3,4,5,6,7,8,9,10,11,12]
-variations = []
+def count_months_variations(months_range):
+  months = list(range(1,months_range+1))
+  variations = []
 
-sInx = 0
-for startM in months:
-  print(sInx, startM)    
-  if sInx != len(months) - 1:
-      for endM in months[sInx+1:]:
-          variations.append('{0}-{1}'.format(startM, endM))
-  sInx = sInx + 1
-  
-print(variations, len(variations))
-# 66 variations in one year
+  sInx = 0
+  for startM in months:
+    if sInx != len(months) - 1:
+        for endM in months[sInx+1:]:
+            variations.append('{0}-{1}'.format(startM, endM))
+    sInx = sInx + 1
     
+  print(variations)
+  return len(variations)
+  
+
+vars_in_year = count_months_variations(12)
+print(vars_in_year) # 66 variations in one year
+
+vars_in_3years = count_months_variations(12*3)
+print(vars_in_3years) # 630 variations in one year
